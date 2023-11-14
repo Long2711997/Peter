29b78eade29dbadc8d135f94cacde9f5b7e01fbb
Your Name <you@example.com>
2023-11-14 12:29:18 -0600

    update responsive

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Carousel extends Component {
   render() {
     return (
       <div className="carousel">
-        <div className="carousel_banner_wrapper">
-          <img className="carouselBanner" src="./longbanner-01 1.png" alt="" />
-        </div>
-        <div className="carousel_text">
-          <div class="content">
-            <div class="content__container">
-              <p class="content__container__text">Hello</p>
-              <ul class="content__container__list">
-                <li class="content__container__list__item">World !</li>
-                <li class="content__container__list__item">Everybody !</li>
-              </ul>
+          <div className="carousel_text">
+            <div class="content">
+              <div class="content__container">
+                <p class="content__container__text">Hello</p>
+                <ul class="content__container__list">
+                  <li class="content__container__list__item">World !</li>
+                  <li class="content__container__list__item">Everybody !</li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
       </div>
     );
   }
